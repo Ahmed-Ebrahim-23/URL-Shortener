9f5c8aa6650f6016c1ee28daa9d66230ec2acb49
Ahmed-Ebrahim-23 <ahmed2322004ahmed@gmail.com>
2026-01-14 01:31:55 +0200

    feat: add cors for frontend integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 const connectDB = require('./config/dbConnection')
@@ -12,6 +13,7 @@ connectDB();
 const app = express()
 const port = process.env.PORT
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
